d633c356e86111204d0c2704c197aeddfc9df7ba
AlexAb123 <alex.abdulhayoglu@gmail.com>
2025-07-29 18:36:28 -0400

    Update camera.h

--- src/camera.h
+++ src/camera.h
@@ -14,7 +14,7 @@ private:
     int     m_imageWidth = 1600;;       // Rendered image width in pixel count
     int     m_imageHeight = 900;        // Rendered image height
     int     m_samplesPerPixel = 10;     // Count of random samples for each pixel
-    double m_pixelSamplesScale = 0.1;         // Color scale factor for a sum of pixel samples
+    double m_pixelSamplesScale = 0.1;   // Color scale factor for a sum of pixel samples
     Vector3 m_center;                   // Camera center
     Vector3 m_pixel00Center;            // Location of pixel 0, 0
     Vector3 m_pixelDeltaU;              // World space offset to pixel to the right
